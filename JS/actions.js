5061597a8378362e82ce222742b6ac65761ea85c
igortadeu <thegeekamikaze@gmail.com>
2020-06-25 16:08:58 -0300

    First commit

--- JS/actions.js
+++ JS/actions.js
@@ -1,25 +1,25 @@
 function darkMode() {
     document.getElementById("article-header").classList.remove("w3-light-grey");
-    document.getElementById("article-header").classList.add("w3-black");
-    document.getElementById("article-header").classList.add("w3-text-grey");
+    document.getElementById("article_header").classList.add("w3-black");
+    document.getElementById("article_header").classList.add("w3-text-grey");
 
-    document.getElementById("article-options").classList.remove("w3-dark-grey");
-    document.getElementById("article-options").classList.add("w3-black");
+    document.getElementById("article_options").classList.remove("w3-dark-grey");
+    document.getElementById("article_options").classList.add("w3-black");
 
-    document.getElementById("article-text").classList.remove("w3-text-dark-grey");
-    document.getElementById("article-text").classList.add("w3-black");
-    document.getElementById("article-text").classList.add("w3-text-grey");
+    document.getElementById("article_text").classList.remove("w3-text-dark-grey");
+    document.getElementById("article_text").classList.add("w3-black");
+    document.getElementById("article_text").classList.add("w3-text-grey");
 }
 
 function fontSizePlus() {
-    var element = document.getElementById("article-text");
+    var element = document.getElementById("article_text");
     var style = window.getComputedStyle(element, null).getPropertyValue("font-size");
     var size = parseFloat(style); 
     element.style.fontSize = (size + 1) + 'px';
 }
 
 function fontSizeMinus() {
-    var element = document.getElementById("article-text");
+    var element = document.getElementById("article_text");
     var style = window.getComputedStyle(element, null).getPropertyValue("font-size");
     var size = parseFloat(style); 
     element.style.fontSize = (size - 1) + 'px';
@@ -35,10 +35,12 @@ function w3_close() {
 
 function showAccordion() {
     var x = document.getElementById("accordion_topics");
+
     if (x.className.indexOf("w3-show") == -1) {
       x.className += " w3-show";
       x.previousElementSibling.className += " w3-theme-d3";
-    } else { 
+    } 
+    else { 
       x.className = x.className.replace(" w3-show", "");
       x.previousElementSibling.className = 
       x.previousElementSibling.className.replace(" w3-theme-d3", "");

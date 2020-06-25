b6835159a14f003c664b9ff5338c9157231c4378
igortadeu <thegeekamikaze@gmail.com>
2020-06-25 12:58:42 -0300

    First commit

--- JS/actions.js
+++ JS/actions.js
@@ -26,10 +26,21 @@ function fontSizeMinus() {
 }
 
 function w3_open() {
-    // document.getElementById("sidebar").style.width = "20%";
     document.getElementById("sidebar").style.display = "block";
 }
 
 function w3_close() {
     document.getElementById("sidebar").style.display = "none";
+}
+
+function showAccordion() {
+    var x = document.getElementById("accordion_topics");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+      x.previousElementSibling.className += " w3-theme-d3";
+    } else { 
+      x.className = x.className.replace(" w3-show", "");
+      x.previousElementSibling.className = 
+      x.previousElementSibling.className.replace(" w3-theme-d3", "");
+    }
 }
